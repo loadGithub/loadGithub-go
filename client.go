3bdf63bc731a25aa785e1f3b84470a5f47188acd
fish <gcslyp@gmail.com>
2020-01-04 17:07:52 +0800

    完成worker内容,saver mongo-base部分完成

--- client.go
+++ client.go
@@ -6,11 +6,16 @@ import (
 	"loadgithub/service"
 	"log"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/apache/pulsar-client-go/pulsar"
 	"github.com/go-redis/redis/v7"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type LoadgithubMto struct {
@@ -20,7 +25,17 @@ type LoadgithubMto struct {
 	Viewer             string `json:"viewer"`
 }
 
+// login FollowingEndCursor FollowerEndCursor order
+type SaveBaseDto struct {
+	Login              string `json:"login"`
+	FollowingEndCursor string `json:"followingEndCursor"`
+	FollowerEndCursor  string `json:"followerEndCursor"`
+	Order              int    `json:"order"`
+}
+
 func main() {
+
+	log.Println("=================================")
 
 	grpcServer := os.Getenv("GRPC_SERVER")
 	log.Println("grpcServer:" + grpcServer)
@@ -28,13 +43,9 @@ func main() {
 
 	pulsarBroker := os.Getenv("PULSAR_BROKER")
 
-	grpcServer = "localhost:5560"
-
 	if grpcServer == "" {
 		grpcServer = "grpcServer:5560"
 	}
-
-	pulsarBroker = "192.168.3.75:6560"
 
 	if pulsarBroker == "" {
 		pulsarBroker = "pulsar:6650"
@@ -55,8 +66,11 @@ func main() {
 
 	defer client.Close()
 
+	ctx := context.Background()
+	// mongoCtx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
 	redisClient := redis.NewClient(&redis.Options{
-		Addr:     "192.168.3.75:6389",
+		Addr:     "localhost:6389",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
@@ -67,15 +81,41 @@ func main() {
 		log.Println("ping redis fail:{}", err)
 	}
 
+	// 设置客户端连接配置
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// 连接到MongoDB
+	mongoClient, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal("==============connect mongodb fail:{}", err)
+	}
+
+	// 检查连接
+	err = mongoClient.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal("============mongo ping fail{}", err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
+	collection := mongoClient.Database("lgh").Collection("workerQueue")
+	// res, err := collection.InsertOne(mongoCtx, bson.M{"name": "pi", "value": 3.14159})
+	// if err != nil {
+	// 	log.Println("===============insert to mongo fail:{}", err)
+	// }
+	// id := res.InsertedID
+
+	// log.Println("insert.mongodb.data.success:{}", id)
+
 	if role == "worker" {
 
 		go func() {
 			consumer, err := client.Subscribe(pulsar.ConsumerOptions{
 				// topic lgh/viewer/{tokener}
+				Topic: "persistent://public/default/default",
 				// Topic: "persistent://lgh/viewer/default",
-				Topic: "persistent://smartoilets/tdb/default",
+				// Topic: "persistent://smartoilets/tdb/default",
 				// message base
-				//1. token  ->  lgh_token_default  --> etcd
+				//1. token  ->  lgh_token_default  --> redis
 				//2. login
 				//3.followingEndCursor
 				//4.followerEndCursor
@@ -87,6 +127,11 @@ func main() {
 				Type:             pulsar.Shared,
 			})
 
+			if err != nil {
+				log.Fatal("pulsar.consume.worker.fail:{}", err)
+				return
+			}
+
 			defer consumer.Close()
 
 			// 建立连接到gRPC服务
@@ -96,6 +141,15 @@ func main() {
 			}
 			// 函数结束时关闭连接
 			defer conn.Close()
+
+			producer, errp := client.CreateProducer(pulsar.ProducerOptions{
+				Topic: "persistent://public/default/mongo-base",
+			})
+			if errp != nil {
+				log.Fatal(errp)
+			}
+
+			defer producer.Close()
 
 			// grpcClient := service.NewGithubLoaderClient(conn)
 			// log.Println("begin grpc ==================")
@@ -150,7 +204,9 @@ func main() {
 				// }
 				// log.Printf("服务端响应: %s", tr.Message)
 
-				token, rerr := redisClient.Get("lgh_token_" + loadgithubMto.Viewer).Result()
+				//TODO viewer 作为topic11
+
+				token, rerr := redisClient.Get("lgh_token").Result()
 				if rerr != nil {
 					log.Println("get token fail:{}|{}", loadgithubMto, rerr)
 					continue
@@ -161,12 +217,152 @@ func main() {
 				// resp, err := grpcClient.QueryFollow(context.Background(), &service.QueryFollowRequest{Login: "liangyuanpeng", Token: "", FollowingEndCursor: "", FollowerEndCursor: ""})
 				if err != nil {
 					log.Fatalf("could not queryFollow: %v", err)
+					continue
 				}
 				log.Printf("queryFollow.result: %s", resp.String())
+				//TODO insert data topic
+				//persistent://lgh/store/{storedb}  default mongodb
+				//TODO worker优化关闭， 考虑消费了topic没有执行行为
 
+				//login FollowingEndCursor FollowerEndCursor order
+
+				//login name updateAt company email
+
+				//关系表  login_cursor{login_followingEndCursor_followerEndCursor} following follower
+
+				for _, following := range resp.GetData().GetUser().GetFollowing().GetNodes() {
+					var saveBaseDto SaveBaseDto
+					saveBaseDto.Login = following.Login
+					saveBaseDto.FollowingEndCursor = ""
+					saveBaseDto.FollowerEndCursor = ""
+
+					jsonstr, err := json.Marshal(&saveBaseDto)
+					if err != nil {
+						fmt.Println(err.Error())
+						continue
+					}
+
+					//msgId
+					if _, err := producer.Send(ctx, &pulsar.ProducerMessage{
+						Payload: []byte(jsonstr),
+					}); err != nil {
+						log.Printf("pulsar.send.fail: %s", err)
+					}
+				}
+
+				for _, following := range resp.GetData().GetUser().GetFollowers().GetNodes() {
+					var saveBaseDto SaveBaseDto
+					saveBaseDto.Login = following.Login
+					saveBaseDto.FollowingEndCursor = ""
+					saveBaseDto.FollowerEndCursor = ""
+
+					jsonstr, err := json.Marshal(&saveBaseDto)
+					if err != nil {
+						fmt.Println(err.Error())
+						continue
+					}
+
+					//msgId
+					if _, err := producer.Send(ctx, &pulsar.ProducerMessage{
+						Payload: []byte(jsonstr),
+					}); err != nil {
+						log.Printf("pulsar.send.fail: %s", err)
+					}
+				}
+
+				var saveBaseDto SaveBaseDto
+				saveBaseDto.Login = loadgithubMto.Login
+				saveBaseDto.FollowingEndCursor = resp.GetData().GetUser().GetFollowing().GetPageInfo().GetEndCursor()
+				saveBaseDto.FollowerEndCursor = resp.GetData().GetUser().GetFollowers().GetPageInfo().GetEndCursor()
+
+				jsonstr, err := json.Marshal(&saveBaseDto)
+				if err != nil {
+					fmt.Println(err.Error())
+					continue
+				}
+
+				//msgId
+				if _, err := producer.Send(ctx, &pulsar.ProducerMessage{
+					Payload: []byte(jsonstr),
+				}); err != nil {
+					log.Printf("pulsar.send.fail: %s", err)
+				}
+				//确认消费
+				consumer.Ack(msg)
 			}
 		}()
 
+	}
+
+	role = "saver"
+	log.Println("role == saver:{}", (role == "saver"))
+
+	if role == "saver" {
+
+		go func() {
+			log.Println("begin saver==========================")
+			//监听pulsar 保存数据到mongodb
+			consumer, err := client.Subscribe(pulsar.ConsumerOptions{
+				// topic lgh/viewer/{tokener}
+				Topic:            "persistent://public/default/mongo-base",
+				SubscriptionName: "my-sub3",
+				Type:             pulsar.Shared,
+			})
+			if err != nil {
+				log.Fatal("pulsar.consume.worker.fail:{}", err)
+				return
+			}
+
+			defer consumer.Close()
+
+			for {
+
+				mongoCtx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+				msg, err := consumer.Receive(context.Background())
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				fmt.Printf("Received message msgId: %#v -- content: '%s'\n",
+					msg.ID(), string(msg.Payload()))
+
+				var saveBaseDto SaveBaseDto
+
+				jsonerr := json.Unmarshal([]byte(msg.Payload()), &saveBaseDto)
+				if jsonerr != nil {
+					log.Println("json parse fail:{}", jsonerr)
+					continue
+				}
+
+				res, err := collection.InsertOne(mongoCtx, saveBaseDto)
+				if err != nil {
+					if strings.Index(err.Error(), "dup key") > 0 {
+						//重复唯一索引，可以确认消费消息
+						log.Println("mongodb.dup key:{}", err)
+					} else {
+						log.Println("===============insert to mongo fail:{}", err)
+						continue
+					}
+				} else {
+					id := res.InsertedID
+					log.Println("insert.mongodb.success:{}", id)
+				}
+
+				//确认消费
+				consumer.Ack(msg)
+
+			}
+		}()
+	}
+
+	if role == "tasker" {
+		//定时load mongodb task放入redis 队列
+	}
+
+	if role == "committer" {
+		//从redis 队列拿数据 放入pulsar
+		//并且将login放入task记录，且加入过期时间，在没过期前不再执行相关task
 	}
 
 	select {}
--- go.mod
+++ go.mod
@@ -3,9 +3,17 @@ module loadgithub
 go 1.13
 
 require (
+	github.com/DataDog/zstd v1.4.4 // indirect
 	github.com/apache/pulsar-client-go v0.0.0-20191227025134-5004b5d81001
 	github.com/go-redis/redis/v7 v7.0.0-beta.4
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/protobuf v1.3.2
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/pquerna/ffjson v0.0.0-20190930134022-aa0246cd15f7
+	github.com/tidwall/pretty v1.0.0 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.2.0
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553
 	google.golang.org/grpc v1.26.0
 )
